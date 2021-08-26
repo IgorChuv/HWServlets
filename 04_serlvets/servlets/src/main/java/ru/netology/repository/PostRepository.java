42c2f686422f6a170a7dec596cb4452f36222aaa
Igor Chuvilov <Igorchuv74@gmail.com>
2021-08-26 16:38:10 +0500

    HomeWork 2.1

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -11,26 +11,32 @@ import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  private AtomicLong id = new AtomicLong(1);
-  private Map<Long, Post> concurrentHashMap = new ConcurrentHashMap<>();
+    private AtomicLong id = new AtomicLong(1);
+    private Map<Long, Post> concurrentHashMap = new ConcurrentHashMap<>();
 
 
+    public List<Post> all() {
+        return Collections.emptyList();
+    }
 
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.empty();
+    }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public synchronized Post save(Post post) {
+        if (post.getId() == 0) {
+            long newId = id.incrementAndGet();
+            concurrentHashMap.put(newId, post);
+        } else {
+            if (!concurrentHashMap.containsKey(post.getId())) {
+                concurrentHashMap.put(post.getId(), post);
+            }
+            concurrentHashMap.replace(post.getId(), post);
+        }
+        return post;
+    }
 
-  public synchronized Post save(Post post) {
-    long newId = id.incrementAndGet();
-    concurrentHashMap.put(newId, post);
-    return post;
-  }
-
-  public void removeById(long id) {
-    concurrentHashMap.remove(id);
-  }
+    public void removeById(long id) {
+        concurrentHashMap.remove(id);
+    }
 }
